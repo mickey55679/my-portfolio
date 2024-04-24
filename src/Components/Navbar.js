9c9175b4b0baea91a054264cbcdcb54e220ae78f
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-24 12:39:01 -0500

    added navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
