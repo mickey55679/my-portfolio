2dd396b963e0263eae923447eca7bd7c5145085b
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-27 14:13:48 -0500

    projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <div>
       <div className="Projects-content">
-        <h1>Welcome to My Website!</h1>
+        <h1>Welcome to my Projects page!</h1>
         <p>This is the Projects content.</p>
       </div>
     </div>
