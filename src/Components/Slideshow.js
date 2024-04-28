696308ed70ecf1f39a3810dd9492a60e04604bdf
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-27 21:05:50 -0500

    button

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -46,8 +46,21 @@ function Slideshow() {
         backgroundColor: "black",
       }}
     >
-      <div className="box">
+      <div className="box" onClick={() => alert("hello")}>
         <p>My name is Michaiah, it is nice to meet you! </p>
+        <p>Click here to get to know me</p>
+
+        <div className="arrow-container">
+          <svg viewBox="0 0 50 100" className="arrow">
+            <path fill="#ffffff" d="M0 0 L25 50 L0 100 Z" />
+          </svg>
+          <svg viewBox="0 0 50 100" className="arrow">
+            <path fill="#ADD8E6" d="M0 0 L25 50 L0 100 Z" />
+          </svg>
+          <svg viewBox="0 0 50 100" className="arrow">
+            <path fill="#ffffff" d="M0 0 L25 50 L0 100 Z" />
+          </svg>
+        </div>
       </div>
       <h2 className="background-size"> </h2>
     </div>
