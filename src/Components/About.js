994e9440914779b74cda35155cd65fffe0c17a7b
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-02 15:01:38 -0500

    color text change

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,27 @@ function About() {
           <img src={imageOfMe} alt="imageOfMe" />
         </div>
         <h3>Software Engineer</h3>
-        <p>this is a paragraph</p>
+        <div className="About-paragraph">
+          <p>
+            I am a seasoned professional with a unique blend of healthcare, real
+            estate, and insurance experience, recently broadening my skill set
+            to include Full Stack Development through my studies at BloomTech.
+            Throughout my program at BloomTech, I've gained proficiency in
+            JavaScript, HTML, CSS, and React, and have applied these skills in
+            numerous coding challenges and collaborative projects. These
+            experiences have honed my problem-solving skills, understanding of
+            web development, and ability to work as part of a diverse team. My
+            diverse background provides me with a unique perspective in tech,
+            allowing me to understand and cater to various industry needs. I am
+            particularly interested in opportunities where I can leverage my
+            Full Stack Development skills in the healthcare, real estate, or
+            insurance sectors to drive digital transformation. In my spare time,
+            I continue to expand my tech knowledge. I am currently seeking
+            opportunities to apply my development skills in a real-world
+            setting, and welcome the chance to connect with like-minded
+            professionals. Feel free to reach out!
+          </p>
+        </div>
       </div>
     </div>
   );
