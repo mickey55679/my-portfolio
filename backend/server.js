a564e439ebbea568b7365705d08c6479a5030ffb
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-11 22:04:13 -0500

    wip

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import emailjs from "emailjs";
+import { SMTPClient } from "emailjs";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
@@ -9,16 +9,16 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/send", (req, res) => {
+app.post("/send", async (req, res) => {
   const { name, email: senderEmail, message } = req.body;
-
-  const server = emailjs.server.connect({
+  // Set up SMTP client
+  const client = new SMTPClient({
     user: process.env.EMAIL_USER,
     password: process.env.EMAIL_PASS,
     host: process.env.EMAIL_HOST,
     ssl: true,
   });
-
+  // Email options
   const mailOptions = {
     text: message,
     from: senderEmail,
@@ -26,21 +26,21 @@ app.post("/send", (req, res) => {
     subject: `Message from ${name}`,
   };
 
-  server.send(mailOptions, (err, message) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send({
-        success: false,
-        message: "Something went wrong. Try again later",
-      });
-    } else {
-      res.send({
-        success: true,
-        message: "Thanks for contacting me. I will get back to you shortly",
-      });
-    }
-  });
+   // Try to send the email
+  try {
+    const sendResult = await client.send(mailOptions);
+    res.send({
+      success: true,
+      message: "Thanks for contacting me. I will get back to you shortly",
+      details: sendResult,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong. Try again later",
+    });
+  }
 });
-
 const port = process.env.PORT || 5001;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));  
