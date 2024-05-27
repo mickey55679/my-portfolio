53ac3906a0528fc5d29bc91d317080349483fbac
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-27 16:58:38 -0500

    updated projects to be clickable link images when added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
-import chuds from './images/Screenshot 2024-05-09 at 6.02.34 PM.png'
+import chuds from "./images/Screenshot 2024-05-09 at 6.02.34 PM.png";
+
 
 const Projects = () => {
   return (
@@ -8,28 +9,52 @@ const Projects = () => {
         <h1>Welcome to my Projects page!</h1>
         <div className="card-container">
           <div className="card">
-            <img src={chuds} alt="Project 1" />
+            <a
+              href="http://www.project1website.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={chuds} alt="Project 1" />
+            </a>
             <div className="card-content">
               <h2>Chuds</h2>
               <p>Description of Project 1</p>
             </div>
           </div>
           <div className="card">
-            <img src='' alt="Project 2" />
+            <a
+              href="http://www.project2website.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src='' alt="Project 2" />
+            </a>
             <div className="card-content">
               <h2>Project 2</h2>
               <p>Description of Project 2</p>
             </div>
           </div>
           <div className="card">
-            <img src='' alt="Project 3" />
+            <a
+              href="http://www.project3website.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src='' alt="Project 3" />
+            </a>
             <div className="card-content">
               <h2>Project 3</h2>
               <p>Description of Project 3</p>
             </div>
           </div>
           <div className="card">
-            <img src='' alt="Project 4" />
+            <a
+              href="http://www.project4website.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src='' alt="Project 4" />
+            </a>
             <div className="card-content">
               <h2>Project 4</h2>
               <p>Description of Project 4</p>
