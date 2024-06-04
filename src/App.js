be42e392b6a54d33de0d6f0c2ae51ce399c0b002
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-04 14:31:47 -0500

    dark mode added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Homepage, Navbar, About, Projects, ContactForm, Footer } from "./Components/index.js";
+import {
+  Homepage,
+  Navbar,
+  About,
+  Projects,
+  ContactForm,
+  Footer,
+} from "./Components/index.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
 function App() {
+  const [isNight, setIsNight] = useState(false);
+
+  const handleToggle = () => {
+    setIsNight(!isNight);
+  };
+
   return (
     <Router>
-      <div className="App">
+      <div className={`App ${isNight ? "night" : "day"}`}>
         <header className="App-header">
           <Navbar />
+          <div className="toggle-container">
+            <input
+              type="checkbox"
+              id="dn-toggle"
+              className="dn-toggle"
+              checked={isNight}
+              onChange={handleToggle}
+            />
+            <label htmlFor="dn-toggle" className="dn-toggle-label">
+              <FontAwesomeIcon
+                icon={faSun}
+                color={isNight ? "#666" : "yellow"}
+              />
+              <FontAwesomeIcon
+                icon={faMoon}
+                color={isNight ? "white" : "#666"}
+              />
+            </label>
+          </div>
         </header>
         <main>
           <Routes>
