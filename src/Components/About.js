1a4adcad87f052e656d9e05fd51ae3504ff89743
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-19 17:24:03 -0500

    wip

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,16 +14,24 @@ function About() {
           <p>
             As a seasoned professional with a unique blend of healthcare, real
             estate, and insurance experience, I recently expanded my skills to
-            Full Stack Development through  <a href="https://www.bloomtech.com">BloomTech</a>. Proficient in JavaScript,
-            HTML, CSS, and React, I've tackled numerous coding challenges and
-            contributed to collaborative projects, honing my problem-solving
-            abilities and teamwork skills. My diverse background enables a
-            unique tech perspective, especially in industries like healthcare,
-            real estate, and insurance, where I aim to drive digital
-            transformation. When not programming, I help on our family farm,
-            blending traditional values with tech innovation. I am keen to apply
-            my development skills in real-world settings and connect with
-            like-minded professionals. Feel free to reach out! 
+            Full Stack Development through{" "}
+            <a
+              href="https://www.bloomtech.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "coral", textDecoration: "none" }}
+            >
+              BloomTech
+            </a>
+            . Proficient in JavaScript, HTML, CSS, and React, I've tackled
+            numerous coding challenges and contributed to collaborative
+            projects, honing my problem-solving abilities and teamwork skills.
+            My diverse background enables a unique tech perspective, especially
+            in industries like healthcare, real estate, and insurance, where I
+            aim to drive digital transformation. When not programming, I help on
+            our family farm, blending traditional values with tech innovation. I
+            am keen to apply my development skills in real-world settings and
+            connect with like-minded professionals. Feel free to reach out!
           </p>
         </div>
       </div>
