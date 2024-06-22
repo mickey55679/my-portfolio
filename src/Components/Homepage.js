6fdf4da12bed5067de6d13dcb3642a1c20693832
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-22 17:52:06 -0500

    wip

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,10 +1,11 @@
+import React from "react";
 import Slideshow from "./Slideshow.js";
-
 
 function Homepage() {
   return (
     <div className="Homepage">
       <Slideshow />
+   
     </div>
   );
 }
