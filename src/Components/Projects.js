0bceff1c034dbafb5a4cf160a842a27e2cefc9e2
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-23 17:10:00 -0500

    wip

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,18 +1,19 @@
 import React from "react";
+import asylum from './images/screenshot.png'
 
 
 const projects = [
   {
     id: 1,
-    title: "Project Title 1",
+    title: "Chuds",
     description: "Description for project 1.",
     image: "image1.jpg",
   },
   {
     id: 2,
-    title: "Project Title 2",
+    title: "Asylum-rg-fe-starter",
     description: "Description for project 2.",
-    image: "image2.jpg",
+    image: asylum,
   },
   {
     id: 3,
@@ -31,15 +32,14 @@ const projects = [
 const Projects = () => {
   return (
     <div className="Projects-content">
-      {/* <h3>My Projects</h3> */}
       <div className="card-container">
         {projects.map((project) => (
           <div className="card" key={project.id}>
             <div className="card-image">
+              <h3>{project.title}</h3>
               <img src={project.image} alt={project.title} />
             </div>
             <div className="card-content">
-              <h3>{project.title}</h3>
               <p>{project.description}</p>
             </div>
           </div>
