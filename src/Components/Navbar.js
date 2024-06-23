80fcaab36a1430c3dc0518384eb9a44ca16bb901
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-23 01:26:03 -0500

    wip

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import myLogoLight from "./images/logomb.png";
 import myLogoDark from "./images/logo2.0.png";
 
 const Navbar = ({ isNight }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
   const smoothScroll = (id) => {
     const element = document.getElementById(id);
     element.scrollIntoView({ behavior: "smooth" });
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
       <div className="logo">
         <Link to="/" onClick={() => smoothScroll("home")}>
           <img src={isNight ? myLogoDark : myLogoLight} alt="logo" />
