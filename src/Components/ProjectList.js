ad16ed7c52fd286704114e6816fe5746a2c24dfe
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-26 14:25:18 -0500

    added images, not sure if keeping

--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -1,30 +1,35 @@
 import React from "react";
 import ProjectCard from "./ProjectCard.js";
+import ktArtistry from "./images/Ktartistry.png";
+import chuds from "./images/chudslogo.jpg"
+import myLogo from "./images/logomb.png"
+import asylumlogo from "./images/Asylumimage.png"
+
 
 
 const ProjectList = () => {
   return (
     <div className="projects-grid-container flex flex-wrap">
       <ProjectCard
-        imageUrl="https://cdn.pixabay.com/photo/2024/03/11/12/43/leaf-8626541_1280.jpg"
+        imageUrl={asylumlogo}
         name="Asylum Project"
         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         githubUrl="https://github.com/mickey55679/asylum-rg-fe-starter"
       />
       <ProjectCard
-        imageUrl="https://cdn.pixabay.com/photo/2024/03/11/12/43/leaf-8626541_1280.jpg"
+        imageUrl={chuds}
         name="Chuds"
         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         githubUrl="https://github.com/mickey55679/chuds"
       />
       <ProjectCard
-        imageUrl="https://cdn.pixabay.com/photo/2024/03/11/12/43/leaf-8626541_1280.jpg"
+        imageUrl={ktArtistry}
         name="Kt-artistry"
         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         githubUrl="https://github.com/mickey55679/kt-artistry"
       />
       <ProjectCard
-        imageUrl="https://cdn.pixabay.com/photo/2024/03/11/12/43/leaf-8626541_1280.jpg"
+        imageUrl={myLogo}
         name="my-portfolio"
         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         githubUrl="https://github.com/mickey55679/my-portfolio"
