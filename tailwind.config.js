c6a1d1af33f9efd7845cd4cf9ceb9f57863d497d
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-28 19:18:50 -0500

    need to fix navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:[ "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      
+      colors: {
+        "primary-color": "#e6e6e6",
+        "secondary-color": "#333",
+        "third-color": "teal",
+        "fourth-color": "#666",
+        "fifth-color": "coral",
+      },
     },
   },
   plugins: [],
-}
+};
 
