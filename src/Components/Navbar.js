a23b760f26ddc8c9beb6f01ff432e54660980c71
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-30 16:29:33 -0500

    need  the scroll thing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
@@ -7,10 +7,27 @@ import myLogoDark from "./images/2.png";
 
 const Navbar = ({ isNight }) => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const isTop = scrollTop > 0;
+      setIsScrolled(isTop);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const smoothScroll = (id) => {
     const element = document.getElementById(id);
-    element.scrollIntoView({ behavior: "smooth" });
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   const handleDropdownToggle = () => {
@@ -18,7 +35,11 @@ const Navbar = ({ isNight }) => {
   };
 
   return (
-    <nav className="navbar fixed w-full flex justify-between items-center px-4 md:px-8 ">
+    <nav
+      className={`navbar ${
+        isScrolled ? "bg-primary-color" : ""
+      } fixed top-0 left-0 w-full flex justify-between items-center px-4 md:px-8 transition-colors duration-300 z-50`}
+    >
       <div className="logo">
         <Link to="/" onClick={() => smoothScroll("home")}>
           <img src={isNight ? myLogoDark : myLogoLight} alt="logo" />
