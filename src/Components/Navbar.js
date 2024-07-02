4a9ecf54bfe12a637362efc29482a9855f62e1d0
Michaiah Bos <michaiahbos@yahoo.com>
2024-07-01 19:14:44 -0500

    sticky navbar dissapear

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,17 +2,24 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import myLogo from "./images/1-removebg-preview.png"; // Assuming myLogo is used for both light and dark modes
+import myLogo from "./images/1-removebg-preview.png"; 
 
 const Navbar = () => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [isScrolling, setIsScrolling] = useState(false);
   const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
-      setIsScrolling(scrollTop > lastScrollTop && scrollTop > 0);
+      if (scrollTop > lastScrollTop && scrollTop > 0) {
+        setIsScrolling(true);
+        setIsVisible(false);
+      } else {
+        setIsScrolling(false);
+        setIsVisible(true);
+      }
       setLastScrollTop(scrollTop);
     };
 
@@ -48,9 +55,9 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`navbar ${
+      className={`navbar fixed top-0 left-0 w-full flex justify-between items-center px-4 md:px-8 transition-all duration-300 z-50 ${
         isScrolling ? "bg-primary-color" : ""
-      } fixed top-0 left-0 w-full flex justify-between items-center px-4 md:px-8 transition-colors duration-300 z-50`}
+      } ${isVisible ? "visible" : "invisible"}`}
     >
       <div className="logo">
         <Link to="/" onClick={() => smoothScroll("home")}>
