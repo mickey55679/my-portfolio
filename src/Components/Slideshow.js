958e9a3e534052253695da064bccaafb1f2eb184
Michaiah Bos <michaiahbos@yahoo.com>
2024-07-02 11:51:31 -0500

    updates to images

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -7,16 +7,16 @@ function Slideshow() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slides = [
     {
-      url: "https://cdn.pixabay.com/photo/2016/11/29/13/21/cow-1869792_1280.jpg",
-      alt: "Slide 1",
+      url: "https://cdn.pixabay.com/photo/2014/06/04/16/44/calf-362170_1280.jpg",
+      alt: "Cow image",
     },
     {
       url: "https://cdn.pixabay.com/photo/2014/06/18/13/56/tractor-371250_1280.jpg",
-      alt: "Slide 2",
+      alt: "Tractor",
     },
     {
       url: "https://cdn.pixabay.com/photo/2022/02/28/15/28/animals-7039472_1280.jpg",
-      alt: "Slide 3",
+      alt: "Brwn cow",
     },
   ];
 
