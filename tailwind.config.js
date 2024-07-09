9a035525cb6d2d0481083af28d48e2453e1a6fd4
Michaiah Bos <michaiahbos@yahoo.com>
2024-07-09 16:27:14 -0500

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,7 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        "primary-color": "#ffffff",
-        "secondary-color": "#a5d961",
-        "third-gr-color": "#c1ff72",
-        "tertiary-color:": "#669999",
-        "blue-color": "#22313f",
-        "pink-color": "#FFC0CB",
-      },
+      // Remove this empty extend block if not needed
     },
   },
   plugins: [],
