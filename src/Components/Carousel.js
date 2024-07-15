da46f1fe0d8247dec1f5f4b6e4cef2d8a5b1be37
Michaiah Bos <michaiahbos@yahoo.com>
2024-07-15 16:44:32 -0500

    added link

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -15,24 +15,28 @@ const CARDS = [
     githubUrl: "https://github.com/mickey55679/asylum-rg-fe-starter",
     imageUrl: asylumImg,
     loomUrl: "https://loom.com/share/folder/36e7bf8bc4a149d79120b1f7b414b0eb",
+    websiteUrl: null,
   },
   {
     title: "Chuds",
     description: "#HTML #CSS #React",
     githubUrl: "https://github.com/mickey55679/chuds",
     imageUrl: chuds,
+    websiteUrl: null,
   },
   {
     title: "Kt-artistry",
     description: "#HTML #CSS #React",
     githubUrl: "https://github.com/mickey55679/kt-artistry",
     imageUrl: ktArtistry,
+    websiteUrl: null,
   },
   {
     title: "my-portfolio",
     description: "#HTML #CSS #React #Jest #Tailwindcss",
     githubUrl: "https://github.com/mickey55679/my-portfolio",
     imageUrl: myPortfolio,
+    websiteUrl: "https://www.michaiahbossoftwareengineer.com/",
   },
 ];
 
@@ -41,6 +45,7 @@ const MAX_VISIBILITY = 4;
 
 const Carousel = () => {
   const [active, setActive] = useState(2);
+
 
 
   const handleClick = (index) => {
@@ -69,12 +74,10 @@ const Carousel = () => {
         {CARDS.map((card, index) => (
           <div
             key={index}
-            // Individual card styles
             className={`absolute w-full h-full transition-all duration-300 ease-out ${
               index === active ? "z-10" : "z-[9]"
             }`}
             style={{
-              // Dynamic CSS variables for animation
               "--offset": (active - index) / 3,
               "--direction": Math.sign(active - index),
               "--abs-offset": Math.abs(active - index) / 3,
@@ -82,13 +85,11 @@ const Carousel = () => {
               opacity: Math.abs(active - index) >= MAX_VISIBILITY ? "0" : "1",
               display:
                 Math.abs(active - index) > MAX_VISIBILITY ? "none" : "block",
-              // Card transformation and blur effect
               transform: `rotateY(calc(var(--offset) * 50deg)) scaleY(calc(1 + var(--abs-offset) * -0.4)) translateZ(calc(var(--abs-offset) * -30rem)) translateX(calc(var(--direction) * -5rem))`,
               filter: `blur(calc(var(--abs-offset) * 2rem))`,
             }}
           >
             <div
-              // Card content styles
               className="relative w-full h-full p-8 rounded-lg text-justify transition-all duration-300 ease-out flex flex-col justify-center items-center"
               style={{
                 backgroundColor: `hsl(95, 100%, calc(100% - var(--abs-offset) * 50%))`,
@@ -96,7 +97,25 @@ const Carousel = () => {
               }}
             >
               <h2 className="card-title">{card.title}</h2>
-              <img className="card-img" src={card.imageUrl} alt={card.title} />
+              {card.websiteUrl ? (
+                <a
+                  href={card.websiteUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    className="card-img"
+                    src={card.imageUrl}
+                    alt={card.title}
+                  />
+                </a>
+              ) : (
+                <img
+                  className="card-img"
+                  src={card.imageUrl}
+                  alt={card.title}
+                />
+              )}
               <p className="custom-paragraph">{card.description}</p>
               {card.loomUrl && (
                 <a
@@ -160,7 +179,7 @@ const Carousel = () => {
               borderRadius: "50%",
               padding: "0.5rem",
               transition: "background-color 0.3s",
-              right: "1rem", 
+              right: "1rem",
             }}
           >
             <TiChevronRightOutline />
