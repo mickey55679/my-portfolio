e47af967346f03602ab9244e4c49fa1405a45a70
Michaiah Bos <michaiahbos@yahoo.com>
2024-11-12 18:29:54 -0600

    fixed config file

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,11 +3,29 @@ import React from "react";
 
 function Blog() {
   return (
-    <div className="blog">
-      <h1>hello</h1>
-
-     
-   
+    <div className="contain flex w-[90%] md:w-[80%] sm:w-[80%] mx-auto justify-between items-center flex-col md:flex-row my-[100px]">
+      <div className="container_body md:w-[60%] w-[80%]">
+        <h2 className="text-3xl font-bold">Its Heading</h2>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Amet magnis
+          ultricies ornare nascetur iaculis. Faucibus dolor curabitur in
+          fermentum lacus proin. Sed blandit quam litora habitant lacus vivamus
+          himenaeos orci auctor. Ultricies gravida libero in semper lacus leo
+          ullamcorper semper. Pharetra litora efficitur in, viverra interdum
+          nisl. Natoque nisl ullamcorper consequat; per orci commodo vehicula.
+          Sodales volutpat nisl conubia quis ipsum per nam metus. Magnis congue
+          erat pharetra vulputate sem mattis? Ridiculus commodo tristique
+          scelerisque volutpat erat egestas felis. Curabitur ornare rutrum
+          sagittis ad iaculis tortor! Interdum blandit dui interdum luctus,
+          felis urna porta facilisi. Sapien ad ornare tellus eros adipiscing;
+          dapibus sagittis. Proin nunc vestibulum purus purus efficitur amet.
+          Imperdiet accumsan maecenas montes, turpis vehicula duis torquent
+          tristique. Dui nullam pulvinar nec ex varius fringilla augue
+          hendrerit. Curae nec taciti eget erat litora felis; adipiscing litora
+          diam.
+        </p>
+      </div>
+      <img src="https://cdn.pixabay.com/photo/2022/05/04/11/49/dried-flowers-7173793_1280.jpg" width="200px" className="rounded-md"/>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      // Remove this empty extend block if not needed
+      colors: {
+        primary: "#ffffff",
+        secondaryGreen: "#a5d961",
+        thirdGreen: "#c1ff72",
+        tertiary: "#669999",
+        blue: "#22313f",
+        pink: "#FFC0CB",
+      },
+      fontFamily: {
+        cormorant: ['"Cormorant Garamond"', "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      keyframes: {
+        wave: {
+          "0%, 60%, 100%": { transform: "rotate(0deg)" },
+          "10%, 30%": { transform: "rotate(14deg)" },
+          "20%, 40%": { transform: "rotate(-8deg)" },
+          "50%": { transform: "rotate(10deg)" },
+        },
+      },
+      animation: {
+        wave: "wave 2s infinite",
+      },
     },
   },
   plugins: [],
 };
+
+export default config;
