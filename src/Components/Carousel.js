7835e9076d51e7ed28ef22d2af9ed27be5842ed5
Michaiah Bos <michaiahbos@yahoo.com>
2025-05-23 18:29:58 -0500

    got rid of kt artistry for now

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -5,7 +5,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import asylumImg from "./images/HRF.png";
 import myPortfolio from "./images/myportfolio.png";
 import chuds from "./images/chuds.png";
-import ktArtistry from "./images/nailSalon.png";
+// import ktArtistry from "./images/nailSalon.png";
 import coachSite from "./images/coaching site.png";
 import wnhaSite from "./images/WHNA.png";
 
